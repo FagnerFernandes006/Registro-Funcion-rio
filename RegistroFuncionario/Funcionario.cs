8961ef41684d44df9e8fcc484fd4d2f53da148ff
Fagner Fernandes <Fagner Fernandes@DESKTOP-ACNLJLV>
2022-06-06 10:23:25 -0300

    RegistroFuncionario

---
+++ RegistroFuncionario/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace RegistroFuncionario
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public int Id { get; set; }
+        public double Salario { get; set; }
+
+        public void AumentaSalario(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100;
+        }
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Nome
+                + ", "
+                + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ RegistroFuncionario/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace RegistroFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos funcionários você irá registrar? ");
+            int qtd = int.Parse(Console.ReadLine());
+            List<Funcionario> list = new List<Funcionario>();
+
+            for (int i = 0; i < qtd; i++)
+            {
+                int posicao = i + 1;
+                Console.WriteLine("Funcionário #" + posicao + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+
+                list.Add(new Funcionario() { Id = id, Nome = nome, Salario = salario });   
+            }
+
+            Console.Write("Entre com o Id do funcionário que terá o salário aumentado: ");
+            int search = int.Parse(Console.ReadLine());
+            Funcionario s1 = list.Find(x => x.Id == search);
+
+            if (s1 == null)
+            {
+                Console.WriteLine("Este Id não existe!");
+                Console.WriteLine();
+                Console.WriteLine("Lista: ");
+            }   
+            else
+            {
+                Console.WriteLine("Você escolheu: " + s1.Nome);
+                Console.Write("Porcentagem: ");
+                double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                s1.AumentaSalario(porcentagem);
+                Console.WriteLine();
+                Console.WriteLine("Lista atualizada: ");
+            }
+            foreach (Funcionario obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
